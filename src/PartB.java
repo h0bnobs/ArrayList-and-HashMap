1ce5e48f8cf8cb2df3a3720f539ec4e31a888c78
h0bnobs <45399095+h0bnobs@users.noreply.github.com>
2023-01-29 18:10:42 +0000

    Update PartB.java
    practice

--- src/PartB.java
+++ src/PartB.java
@@ -15,23 +15,39 @@ public class PartB
                 "Good enough for anyone/Else to hear/" +
                 "Just sing, sing a song";
         System.out.println("Original lyrics: " + lyrics);
+
         String newLyrics = lyrics.replace("/", " ");
         String newerLyrics = newLyrics.replace(",", "");
-        String newestLyrics = newerLyrics.replace("'", "");
+        String newestLyrics = newerLyrics.replace("'", "").trim().toLowerCase();
 
         HashMap<String, Integer> map = new HashMap<>();//create hashmap
         String[] arr = newestLyrics.split(" ");//convert String newestLyrics to an array
-        for(int i=0; i<arr.length; i++) {//could be made into a for each.
-            if(map.containsKey(arr[i])) {//if the hashmap DOES contain the word then find its value
-                int count = map.get(arr[i]);//which is int count and add 1 to it again.
-                map.put(arr[i],count + 1);//put this word into the map with the updated value of its count.
-            }
-            else {
-                map.put(arr[i],1);//if the word is not yet in the hashmap, meaning if it is the first
+
+        for (String s : arr) {//could be made into a for each.
+            if (map.containsKey(s)) {//if the hashmap DOES contain the word then find its value
+                int count = map.get(s);//which is int count and add 1 to it again.
+                map.put(s, count + 1);//put this word into the map with the updated value of its count.
+            } else {
+                map.put(s, 1);//if the word is not yet in the hashmap, meaning if it is the first
                 //time it has been found, put it in and assign its value (number of occurences) to 1
             }
         }
-        System.out.println("Count of words in those lyrics: "+ map);
+
+        int max = 0;
+        for (int freq: map.values()) {
+            if (freq > max) {
+                max = freq;
+            }
+        }
+        System.out.println("Number of words total: " + arr.length);
+        System.out.println("Count of words in those lyrics: " + map);
+        System.out.println("The max frequency is " + max);
+        System.out.println("Words with the max frequency are:");
+        for (String word: map.keySet()) {
+            if (map.get(word).equals(max)) {
+                System.out.print(word + " ");
+            }
+        }
     }
 }
 
